70c8e279c22487e38e5dde7ffe78b607ac0178ab
KnightWhoSaysNi <nnikola.milovanovic@gmail.com>
2017-05-12 14:45:52 +0200

    AnswerPanel script with OnDisable added to AnswerPanel

---
+++ Assets/Scripts/UI/AnswerPanel.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AnswerPanel : MonoBehaviour
+{
+    public GameObject hintOptions;
+    public GameObject hideHintOptions;
+    public GameObject showHintOptions;
+
+    [Space(10)]
+    public GameObject locationOptions;
+    public GameObject hideLocationOptions;
+    public GameObject showLocationOptions;
+
+    [Space(5)]
+    public InputField latitude;
+    public InputField longitude;
+
+    private void OnDisable()
+    {
+        hintOptions.SetActive(false);
+        hideHintOptions.SetActive(false);
+        showHintOptions.SetActive(true);
+
+        locationOptions.SetActive(false);
+        hideLocationOptions.SetActive(false);
+        showLocationOptions.SetActive(true);
+
+        latitude.text = string.Empty;
+        longitude.text = string.Empty;
+    }
+}
