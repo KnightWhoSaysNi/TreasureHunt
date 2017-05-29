7bfeebaeedb8559c433a3cbed6cfb9e9d8e03e84
KnightWhoSaysNi <nnikola.milovanovic@gmail.com>
2017-05-30 00:40:05 +0200

    PasswordPanel added

---
+++ Assets/Scripts/UI/Miscellaneous/PasswordPanel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PasswordPanel : MonoBehaviour
+{
+    public InputField[] passwordInputFields;    
+
+    private void OnDisable()
+    {
+        foreach (InputField passwordInputField in passwordInputFields)
+        {
+            passwordInputField.text = string.Empty;
+        }
+    }
+}
